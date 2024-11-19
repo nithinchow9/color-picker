cfc5281d4c3287b8749034dcc3392cf801b8cd49
Nithin Chowdary <118511903+nithinchow9@users.noreply.github.com>
2024-11-19 13:31:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ColorPicker from './Components/Colorpicker';
+
+function App() {
+  return (
+    <main>
+      <ColorPicker />
+    </main>
+  );
+}
+
+export default App;
